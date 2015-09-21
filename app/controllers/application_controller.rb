a92796069bc2b4fcbbf0421ae0ce70912461d8c9
gil93 <gvelezjr02@gmail.com>
2015-09-21 11:46:33 -0400

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  helper_method :current_user 
+
+  def current_user 
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end
+
+  def require_user 
+    redirect_to('/login', format: :html) unless current_user 
+  end
+end
+
+# before_action :require_user, only: [:action1, :action2] 
+# place above code on access_only pages
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController 
+	def new; end
+
+	def create
+  	  @user = User.find_by_email(params[:session][:email])
+  	  if @user && @user.authenticate(params[:session][:password])
+    	session[:user_id] = @user.id
+   		redirect_to(root_url, format: :html)
+  	  else
+    	redirect_to('/login', format: :html)
+  	  end 
+	end
+
+	def destroy 
+  	  session[:user_id] = nil 
+  	  redirect_to(root_url, format: :html) 
+	end
+end
