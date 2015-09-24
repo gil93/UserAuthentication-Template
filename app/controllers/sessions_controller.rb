a75623ae7f7273efaab0ef54a59385c7122a239d
Gilberto Velez <gilbertovelez@Gilbertos-MBP.home>
2015-09-23 20:47:40 -0400

    refacor, experiment with sass and asset pipeline

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,18 @@
-class SessionsController < ApplicationController 
+class SessionsController < ApplicationController
 	def new; end
 
 	def create
   	  @user = User.find_by_email(params[:session][:email])
   	  if @user && @user.authenticate(params[:session][:password])
-    	session[:user_id] = @user.id
-   		redirect_to(root_url, format: :html)
+	    	session[:user_id] = @user.id
+	   		redirect_to(root_url, format: :html)
   	  else
-    	redirect_to('/login', format: :html)
-  	  end 
+	    	redirect_to('/login', format: :html)
+  	  end
 	end
 
-	def destroy 
-  	  session[:user_id] = nil 
-  	  redirect_to(root_url, format: :html) 
+	def destroy
+  	  session[:user_id] = nil
+  	  redirect_to(root_url, format: :html)
 	end
 end
